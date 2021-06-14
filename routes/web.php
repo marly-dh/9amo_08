b11080f1ca0e773191bd55d6224232efd07fa843
Caffeine Boy <christian.aartman@gmail.com>
2021-06-14 11:48:49 +0200

    init posts

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'RouteController@homePage');
+Route::get('/posts', 'RouteController@posts');
 Route::any('/{route}', 'RouteController@index')->where('route', '(.*)');;
